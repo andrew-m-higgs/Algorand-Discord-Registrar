c5c8113158ee6888e1ce44eb40e2b1affd54e2e0
Andrew Higgs <andrew.m.higgs@gmail.com>
2023-03-13 12:25:41 +0200

    Add command /config-owner-roles.js

--- functions/events/discord/command/config-owner-roles.js
+++ functions/events/discord/command/config-owner-roles.js
@@ -3,5 +3,96 @@ console.log('        command/config-owner-roles.js STARTING...')
 console.log('################################################################################')
 
 const lib       = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
-const config    = require('../../../../helpers/config.js');
 const functions = require('../../../../helpers/functions.js');
+
+// Get general information
+let guild_id       = context.params.event.guild_id;
+let token          = context.params.event.token;
+let newOwnerRoleID = context.params.event.data.options[0].value;
+let newNumNFTs     = context.params.event.data.options[1].value;
+
+// Set some general information needed
+let newOwnerRoleName = await functions.getRoleName(context, newOwnerRoleID);
+let newOwnerRoles    = [];
+
+// Set variables up for interaction message
+let message_content = "";
+let embeds          = [];
+let components      = [];
+let notPushed       = true;
+
+// SET FAKE DATA TO TEST
+//let embeds = [{"type":"rich","title":"15 or more NFTs needed","description":"","color":7909721,"fields":[{"name":"Rolename","value":"budfather15+","inline":true},{"name":"NFTs","value":"15","inline":true},{"name":"RoleID","value":"1075738013734281246","inline":true}],"footer":{"text":"This role is being added or updated."}}];
+//let components      = [{"type":1,"components":[{"style":3,"label":"Update","custom_id":"btn_roles_update","disabled":false,"type":2},{"style":4,"label":"Cancel","custom_id":"btn_roles_cancel","disabled":false,"type":2}]}];
+
+// Check if member is an admin
+let isAdmin = await functions.isAdmin(context);
+
+if (isAdmin) {
+// IS AN ADMIN
+  let currentOwnerRoles = await lib.utils.kv['@0.1.16'].get({
+    key: "OwnerRoles-" + guild_id,
+  });
+  
+  if (currentOwnerRoles == null) {
+    currentOwnerRoles = [];
+  };
+
+  //console.log("Current Owner Roles : " + JSON.stringify(currentOwnerRoles));
+
+  if (currentOwnerRoles.length > 0) {
+    //console.log(`Got here: A`);
+    for (let i = 0; i < currentOwnerRoles.length; i++) {
+      //console.log(`Got here: B`);
+      if (currentOwnerRoles[i].amount < newNumNFTs) {
+        let embed = functions.setOwnerRoleEmbed(newOwnerRoleID, newOwnerRoleName, newNumNFTs, "ADD");
+        embeds.push(embed);
+        newOwnerRoles.push({
+          "id": newOwnerRoleID,
+          "amount": newNumNFTs
+        });
+        notPushed = false;
+      }
+      let roleID   = currentOwnerRoles[i].id;
+      let roleName = await functions.getRoleName(context, roleID);
+      let numNFTs  = currentOwnerRoles[i].amount;
+      let embed    = functions.setOwnerRoleEmbed(roleID, roleName, numNFTs, "KEEP");
+      embeds.push(embed);
+      newOwnerRoles.push({
+        "id": roleID,
+        "amount": numNFTs
+      });
+    }
+  }
+  if (notPushed) {
+    let embed = functions.setOwnerRoleEmbed(newOwnerRoleID, newOwnerRoleName, newNumNFTs, "ADD");
+    newOwnerRoles.push({
+      "id": newOwnerRoleID,
+      "amount": newNumNFTs
+    });
+    embeds.push(embed);
+  }
+  message_content = "The owner roles are as follows: "
+  components      = [{"type":1,"components":[{"style":3,"label":"Update","custom_id":"btn_roles_update","disabled":false,"type":2},{"style":4,"label":"Cancel","custom_id":"btn_roles_cancel","disabled":false,"type":2}]}];
+} else {
+  // NOT AN ADMIN
+  message_content = "You do not have permission to run this command. Run the command again after being given the correct role. :-)"
+}
+
+// Persist data
+await lib.utils.kv['@0.1.16'].set({
+  key: "tmp_OwnerRoles-" + guild_id,
+  value: newOwnerRoles
+})
+
+//console.log(JSON.stringify(embeds));
+//console.log(JSON.stringify(newOwnerRoles));
+
+let messageInner = {
+  token: token,
+  content: message_content,
+  embeds: embeds,
+  components: components,
+}
+
+await lib.discord.interactions['@1.0.1'].followups.ephemeral.create(messageInner)
